affabc3ccba63cfb57a47502b327692b387fb58b
favibe <ibefavour85@gmail.com>
2025-10-28 17:04:29 +0100

    type annotation

--- index.dart
+++ index.dart
@@ -13,8 +13,29 @@ void main() {
   print(age +1);
   
   print('my name is $name');
-  
-  
-  
-  
+
+//Type Annotations
+String city = "new york";
+  city = "los angeles";
+  print(city);
+  int num = 34;
+  print(num);
+
+  bool isNow = true;
+  isNow = false;
+  print(isNow);
+
+  double price = 10.5;
+  price = 11.4;
+  print(price);
+
+  //nukllable value
+  int point;
+  print(point); //null(throws an error)
+
+  int? score;
+  print(score); //null (no error)
+
 }
+
+

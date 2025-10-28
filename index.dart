28b332245eb502461a16c23bb37920cf99e8f002
favibe <ibefavour85@gmail.com>
2025-10-28 16:50:31 +0100

    dart variable

---
+++ index.dart
@@ -0,0 +1,20 @@
+//void main
+//void main() {
+
+//}
+
+//dart variables
+void main() {
+  var name = "mario";
+   print(name);
+  
+  const age = 12;
+    print(age);
+  print(age +1);
+  
+  print('my name is $name');
+  
+  
+  
+  
+}

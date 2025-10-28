85cccdb4e5b3c1a49041ffabf9b2c961387bcd26
favibe <ibefavour85@gmail.com>
2025-10-28 18:43:29 +0100

    function

--- index.dart
+++ index.dart
@@ -36,6 +36,41 @@ String city = "new york";
   int? score;
   print(score); //null (no error)
 
+  //functions
+  final greeting = greet('luigi', 30);
+  print(greeting);
+
+  //passing name functions with named parameters
+  final greeting2 = greet2(name: 'yoshi', age: 40);
+  print(greeting2);
+
+  //Ex2
+  final greeting3 = greet3();
+  print(greeting3);
+  
+  //Ex 3
+  final greeting4 = greetUser(name: 'peach');
+  print(greeting4);
+  final greeting5 = greetUser(age: 22, name: 'Elle');
+  print(greeting5);
+
 }
 
+//functions
+greet (String name, int age) {
+  return 'hello $name, you are $age years old';
+}
+//named function parameters
 
+
+greet2 ({required String name, required int age}) {
+  return 'hello $name, you are $age years old';
+}
+//Ex2 
+greet3 ({String name = 'guest', int age = 0}) {
+  return 'hello $name, you are $age years old';
+}
+//Ex 3
+String greetUser({required String name, int? age}) {
+  return 'Hello $name, you are $age years old.';
+}
